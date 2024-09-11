e9a52e60ac37b6aa7a15da47c99c49d5d85f373d
Github MMI3 <github@mmi-troyes.fr>
2024-09-11 13:28:07 +0000

    feat:Slugify add

--- wr506d-mmi22/src/Controller/DemoController.php
+++ wr506d-mmi22/src/Controller/DemoController.php
@@ -5,16 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Cocur\Slugify\Slugify;
 
 class DemoController extends AbstractController
 {
     #[Route('/demo', name: 'app_demo')]
     public function index(): Response
     {
-	$date = new \DateTime('now', new \DateTimeZone('Europe/Paris'));
-        //--test
+        $timezone = new \DateTimeZone('Europe/Paris');
+        $now = new \DateTime('now', $timezone);
+
+        // Définir la localisation en français
+        setlocale(LC_TIME, 'fr_FR.UTF-8');
+
+        $slug = new Slugify();
+        $textSlugified = $slug->slugify('Do the harlem shake', ["separator" => " "]);
+
         return $this->render('demo/index.html.twig', [
-            'date' => $date,
+            'controller_name' => 'DemoController',
+            'current_day' => strftime('%A', $now->getTimestamp()), // Jour de la semaine en français
+            'current_dayN' => $now->format('d'), // Date au format AAAA-MM-JJ
+            'current_month' => strftime('%B', $now->getTimestamp()), // Mois en français
+            'current_time' => $now->format('H:i:s'), // Heure au format HH:MM:SS
+            'textSlugified' => $textSlugified
         ]);
     }
 }
